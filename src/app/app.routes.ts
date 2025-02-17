70216627718e7d5fa1bc8a2283d8424f39f1425e
Fernando <nando.nas@gmail.com>
2025-02-16 22:49:11 -0300

    Added new url test.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PageComponent } from './components/page/page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'teste', 
+    component: PageComponent
+}];
