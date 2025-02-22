62780325801175b77d944b210d97c027a769d4b3
Fernando <nando.nas@gmail.com>
2025-02-22 14:16:06 -0300

    Added git push

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
 
-# Nome do contêiner que queremos parar (deve ser o mesmo do docker-compose.yml)
 CONTAINER_NAME="project-base"
 
-# Verifica se o contêiner está rodando
+echo "Atualizando o repositório..."
+git pull
+
 if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
     echo "Parando o contêiner existente..."
     docker stop $CONTAINER_NAME
     docker rm $CONTAINER_NAME
 fi
 
-# Rodar o docker-compose
+echo "Construindo e iniciando o novo contêiner..."
 docker compose up --build -d
