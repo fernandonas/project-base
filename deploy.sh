6b117d99bd1e7e3747ff8f57ffd7073a5b85c170
Fernando <nando.nas@gmail.com>
2025-02-22 14:36:34 -0300

    fix

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+
+CONTAINER_NAME="project-base"
+
+echo "Atualizando o repositório..."
+sudo git checkout .w
+sudo git pull
+
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    echo "Parando o contêiner existente..."
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+fi
+
+echo "Construindo e iniciando o novo contêiner..."
+docker compose up --build -d
