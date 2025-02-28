c09140eca65857be1bdeb42b3a7050c08a788577
Fernando <nando.nas@gmail.com>
2025-02-27 21:20:09 -0300

    Added ng zorro

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorHandlerInterceptor } from './interceptors/error-handler.interceptor';
 import { authInterceptor } from './interceptors/http.interceptor';
+import { pt_BR, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+registerLocaleData(pt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +19,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(
       withInterceptors([errorHandlerInterceptor, authInterceptor])
-    )
+    ), provideNzI18n(pt_BR), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()
   ]
 };
